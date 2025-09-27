7e2629c07193d32aa6a387a2ef660dfc7d251ae2
Mariano Jara <mjaravillasana@gmail.com>
2025-09-27 11:52:11 -0600

    Commit inicial, incluye estructura básica de login screen y clase de la imagen del login, además estructura de archivos y carpetas que se utilizarán

---
+++ app/src/main/java/com/example/loginapp/Screens/LoginScreen.kt
@@ -0,0 +1,15 @@
+package com.example.loginapp.Screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import coil.compose.AsyncImage
+
+@Composable
+fun LoginScreen(){
+    Column {
+        AsyncImage(
+            model = null
+        )
+
+    }
+}
---
+++ app/src/main/java/com/example/loginapp/classes/LoginImage.kt
@@ -0,0 +1,7 @@
+package com.example.loginapp.classes
+
+data class LoginImage(
+    val url:String,
+    val description:String
+)
+
