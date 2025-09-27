fb301ee4785cde8c055c085bb48f46ef4a351298
Mariano Jara <mjaravillasana@gmail.com>
2025-09-27 12:03:12 -0600

    Comencé a hacer la estructura de mi DataComponent y la de mi LoginScreen

--- app/src/main/java/com/example/loginapp/Components/DataComponent.kt
+++ app/src/main/java/com/example/loginapp/Components/DataComponent.kt
@@ -1,2 +1,30 @@
 package com.example.loginapp.Components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DataComponent(){
+    Row {
+        Column {
+            Text(text = "Email")
+
+            var email by remember { mutableStateOf("") }
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Correo electrónico") },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/loginapp/Screens/LoginScreen.kt
+++ app/src/main/java/com/example/loginapp/Screens/LoginScreen.kt
@@ -1,15 +1,36 @@
 package com.example.loginapp.Screens
-
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import coil.compose.AsyncImage
+import com.example.loginapp.Components.DataComponent
+import com.example.loginapp.classes.LoginImage
 
+val image  = LoginImage(
+    url = " ",
+    description = " "
+)
 @Composable
 fun LoginScreen(){
     Column {
         AsyncImage(
-            model = null
+            model = image.url,
+            contentDescription = image.description,
+            modifier = Modifier
         )
+
+        Column {
+            Text(text = "Login")
+            DataComponent()
+        }
 
     }
 }
